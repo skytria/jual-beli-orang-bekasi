0c4368bfe2c0087468a7e06491e631d1bbbae18b
skytria <vloid137@gmail.com>
2025-08-11 12:07:57 +0700

    update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const inputNama = document.querySelector(".masukan-nama");
 const inputPassword = document.querySelector(".masukan-password");
 const tombolLogin = document.querySelector(".tombol-login")
 
-tombolLogin.addEventListener("click",() => {
+tombolLogin.addEventListener("click",(e) => {
+  e.preventDefault();
   const nama = inputNama.value;
   const password = inputPassword.value;
   console.log(nama);
